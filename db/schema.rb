064f0f90eed2e71e8be57be124c4827e87612a41
madaawadh <madaawadh1@gmail.com>
2018-11-11 14:21:49 +0300

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_081702) do
+ActiveRecord::Schema.define(version: 2018_11_11_110024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_11_11_081702) do
     t.integer "max"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "meetings_tags", id: false, force: :cascade do |t|
